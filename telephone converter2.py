483c2c4aae20128152066148c2e08bccc957f949
garnishundle <91385936+garnishundle@users.noreply.github.com>
2021-10-19 17:32:13 +0100

    Add files via upload

---
+++ telephone converter2.py
@@ -0,0 +1,20 @@
+import random
+
+randint1 = random.randint(0,9)
+randint2 = random.randint(0,9)
+randint3 = random.randint(0,9)
+randint4 = random.randint(0,9)
+randint5 = random.randint(0,9)
+randint6 = random.randint(0,9)
+randint7 = random.randint(0,9)
+randint8 = random.randint(0,9)
+randint9 = random.randint(0,9)
+randint10 = random.randint(0,9)
+randint11 = random.randint(0,9)
+
+
+input("Enter eleven characters (Will only accept first eleven or less):\n")
+
+
+
+print("Your characters are stored as the phone number",randint1,randint2,randint3,randint4,"-",randint5,randint6,randint7,"-",randint8,randint9,randint10,randint11)
